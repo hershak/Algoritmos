e61fbcec1035cb700f2183685ea88861f8d9e8bb
hershak <gael.houl@gmail.com>
2018-01-17 09:59:48 -0500

    Finished Rombo

--- Ejercicios/Arbol.js
+++ Ejercicios/Arbol.js
@@ -1,29 +1,29 @@
 function Arbol() {
-    var ref = '\n';
-    for (var i = 0; i < 10; i++) { //Es la cabeza del árbol
-        for (var j = (10 - i); j > 1; j--) { //Espacios en blanco para que pueda tener la forma de un triángulo isosceles
-            ref += ' ';
-        }
-        for (var k = 1; k <= ((2 * i) + 1); k++) { // dibujando el triángulo isosceles
-            ref += '*';
-        }
-        ref += '\n';
+  var ref = '\n';
+  for (var i = 0; i < 10; i++) { //Es la cabeza del árbol.
+    for (var j = (10 - i); j > 1; j--) { //Espacios en blanco para que pueda tener la forma de un triángulo isosceles.
+      ref += ' ';
     }
-    for (var t = 1; t <= 9; t++) { //la longitud del tallo es la cantidad de iteraciones
-        for (var r = 1; r <= 9; r++) { //nueve uniddades de espacio para dibujar el tallo
-            ref += ' ';
-        }
-        ref += '* \n';
+    for (var k = 1; k <= ((2 * i) + 1); k++) { // dibujando el triángulo isosceles.
+      ref += '*';
     }
-    for (var ra = 1; ra <= 5; ra++) { // espacio en blanco antes de la raíz
-        ref += ' ';
+    ref += '\n';
+  }
+  for (var t = 1; t <= 9; t++) { //la longitud del tallo es la cantidad de iteraciones.
+    for (var r = 1; r <= 9; r++) { //nueve uniddades de espacio para dibujar el tallo.
+      ref += ' ';
     }
-    for (var rb = 1; rb <= 9; rb++) { //la raiz dibujadas
-        ref += '*';
-    }
-    return ref;
+    ref += '* \n';
+  }
+  for (var ra = 1; ra <= 5; ra++) { // espacio en blanco antes de la raíz.
+    ref += ' ';
+  }
+  for (var rb = 1; rb <= 9; rb++) { //la raiz dibujadas.
+    ref += '*';
+  }
+  return ref;
 }
 
-// Imprimiendo resultados
+// Imprimiendo resultados.
 var a = Arbol();
 console.log(a);
--- Ejercicios/Rombo.js
+++ Ejercicios/Rombo.js
@@ -1,25 +1,27 @@
 function Rombo() {
   var n = 5;
   var ref = '';
-  for (var i = 0; i < n; i++) {
-    for (var b = (n - i); b > 1; b--) {
+  for (var i = 0; i < n; i++) { //Parte superior del rombo, considero hasta la fila 5.
+    for (var b = (n - i); b > 1; b--) { //Espacios en blanco en la parte superior.
       ref += ' ';
     }
-    for (var j = 1; j <= ((2 * i) + 1); j++) {
+    for (var j = 1; j <= ((2 * i) + 1); j++) { // Asteriscos en la parte superior.
       ref += '*';
     }
     ref += '\n';
   }
-  for (var i = 4; i > 0; i--) {
-    for (var b = 0; b < 5 - i; b++) {
+  for (var i = 4; i > 0; i--) { // Parte inferior, considero las 4 filas restantes.
+    for (var b = 0; b < 5 - i; b++) { // Espacios en blanco en la parte inferior.
       ref += ' ';
     }
-    for (var j = 0; j < ((i * 2) - 1); j++) {
+    for (var j = 0; j < ((i * 2) - 1); j++) { // Asteriscos en la parte inferior.
       ref += '*';
     }
     ref += '\n';
   }
   return ref;
 }
+
+// Imprimiendo resultados.
 var r = Rombo();
 console.log(r);
