65644e31e3c4069ba78aba042fae5a32051358d1
ridixcr <pabch.04.06.89@gmail.com>
2018-01-17 00:04:38 -0500

    refactoring

--- Ejercicios/Arbol.js
+++ Ejercicios/Arbol.js
@@ -1,28 +1,29 @@
 function Arbol() {
-  var n = 10;
-  var ref = '';
-  for (var i = 0; i < n; i++) { //Es la cabeza del árbol
-    for (var c = (n - i); c > 1; c--) { //Espacios en blanco para que pueda tener la forma de un triángulo isosceles
-      ref += ' ';
+    var ref = '\n';
+    for (var i = 0; i < 10; i++) { //Es la cabeza del árbol
+        for (var c = (10 - i); c > 1; c--) { //Espacios en blanco para que pueda tener la forma de un triángulo isosceles
+            ref += ' ';
+        }
+        for (var j = 1; j <= ((2 * i) + 1); j++) { // dibujando el triángulo isosceles
+            ref += '*';
+        }
+        ref += '\n';
     }
-    for (var j = 1; j <= ((2 * i) + 1); j++) { // dibujando el triángulo isosceles
-      ref += '*';
+    for (var t = 1; t <= 9; t++) { //la longitud del tallo es la cantidad de iteraciones
+        for (var r = 1; r <= 9; r++) { //nueve uniddades de espacio para dibujar el tallo
+            ref += ' ';
+        }
+        ref += '* \n';
     }
-    ref += '\n';
-  }
-  for (var t = 0; t < (n - 1); t++) { //la longitud del tallo es la cantidad de iteraciones
-    for (var r = 1; r < n; r++) { //nueve uniddades de espacio para dibujar el tallo
-      ref += ' ';
+    for (var ra = 1; ra <= 5; ra++) { // espacio en blanco antes de la raíz
+        ref += ' ';
     }
-    ref += '* \n';
-  }
-  for (var ra = 0; ra < 5; ra++) { // espacio en blanco antes de la raíz
-    ref += ' ';
-  }
-  for (var ra = 1; ra < 10; ra++) { //la raiz dibujadas
-    ref += '*';
-  }
-  return ref;
+    for (var rb = 1; rb <= 9; rb++) { //la raiz dibujadas
+        ref += '*';
+    }
+    return ref;
 }
+
+// Imprimiendo resultados
 var a = Arbol();
 console.log(a);
