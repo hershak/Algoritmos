e94e93546f6beb27b1b39e41dfbab702ebb25dd8
hershak <gael.houl@gmail.com>
2018-01-16 21:03:25 -0500

    Adding exercices

--- Ejercicios/Arbol.js
+++ Ejercicios/Arbol.js
@@ -1,7 +1,12 @@
 function Arbol(n) {
   var ref = '';
+  var space ='';
   for (var i = 0; i < n; i++) {
-    for (var j = 1; j < ((2 * i) + 1); j++) {
+    for (var c = n-1; c > 0; c--) {
+      space += ' ';
+    }
+    ref+=space;
+    for (var j = 1; j <= ((2 * i) + 1); j++) {
       ref += '*';
     }
     ref += '\n';
