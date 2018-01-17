8ebdd15e674c54a508a42d9fb1eb7a656d13c571
hershak <gael.houl@gmail.com>
2018-01-16 21:47:15 -0500

    Finished

--- Ejercicios/Arbol.js
+++ Ejercicios/Arbol.js
@@ -1,24 +1,24 @@
 function Arbol(n) {
     var ref = '';
-    for (var i = 0; i < n; i++) {
-        for (var c = (n - i); c > 0; c--) {
-            ref += '-';
+    for (var i = 0; i < n; i++) { //Es la cabeza del árbol
+        for (var c = (n - i); c > 1; c--) { //Espacios en blanco para que pueda tener la forma de un triángulo isosceles
+            ref += ' ';
         }
-        for (var j = 1; j <= ((2 * i) + 1); j++) {
+        for (var j = 1; j <= ((2 * i) + 1); j++) { // dibujando el triángulo isosceles
             ref += '*';
         }
         ref += '\n';
     }
-    for (var t = 0; t < (n - 1); t++) {
-        for (var r = 0; r < n; r++) {
+    for (var t = 0; t < (n - 1); t++) { //la longitud del tallo es la cantidad de iteraciones
+        for (var r = 1; r < n; r++) { //nueve uniddades de espacio para dibujar el tallo
             ref += ' ';
         }
         ref += '* \n';
     }
-    for (var ra = 0; ra < 5; ra++) {
-        ref += '-';
+    for (var ra = 0; ra < 5; ra++) { // espacio en blanco antes de la raíz
+        ref += ' ';
     }
-    for (var ra = 1; ra < 9; ra++) {
+    for (var ra = 1; ra < n; ra++) { //la raiz dibujadas
         ref += '*';
     }
     return ref;
