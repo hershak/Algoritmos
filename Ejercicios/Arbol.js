fbe3ce221068788918b3a2b337b231cb0251fdf2
hershak <gael.houl@gmail.com>
2018-01-17 11:35:52 -0500

    Christmas tree finished

--- Ejercicios/Arbol.js
+++ Ejercicios/Arbol.js
@@ -1,7 +1,8 @@
 function Arbol() {
+  var n=10;
   var ref = '\n';
-  for (var i = 0; i < 10; i++) { //Es la cabeza del árbol.
-    for (var j = (10 - i); j > 1; j--) { //Espacios en blanco para que pueda tener la forma de un triángulo isosceles.
+  for (var i = 0; i < n; i++) { //Es la cabeza del árbol.
+    for (var j = (n - i); j > 1; j--) { //Espacios en blanco para que pueda tener la forma de un triángulo isosceles.
       ref += ' ';
     }
     for (var k = 1; k <= ((2 * i) + 1); k++) { // dibujando el triángulo isosceles.
@@ -9,8 +10,8 @@ function Arbol() {
     }
     ref += '\n';
   }
-  for (var t = 1; t <= 9; t++) { //la longitud del tallo es la cantidad de iteraciones.
-    for (var r = 1; r <= 9; r++) { //nueve uniddades de espacio para dibujar el tallo.
+  for (var t = 1; t < n; t++) { //la longitud del tallo es la cantidad de iteraciones.
+    for (var r = 1; r < n; r++) { //nueve uniddades de espacio para dibujar el tallo.
       ref += ' ';
     }
     ref += '* \n';
@@ -18,7 +19,7 @@ function Arbol() {
   for (var ra = 1; ra <= 5; ra++) { // espacio en blanco antes de la raíz.
     ref += ' ';
   }
-  for (var rb = 1; rb <= 9; rb++) { //la raiz dibujadas.
+  for (var rb = 1; rb < n; rb++) { //la raiz dibujadas.
     ref += '*';
   }
   return ref;
---
+++ Ejercicios/ArbolNavidad.js
@@ -0,0 +1,42 @@
+function ArbolNavidad(){
+    var n1=n2=n3=7;
+    var ref='';
+    for(i=0;i<n1;i++){// Primer nivel del árbol.
+        for(b=((n1+6)-i);b>0;b--){ // Trece espacios en blanco. En este nivel el espacio tienen que ser más.
+            ref+=' ';
+        }
+        for(a=0;a<((2*i)+1);a++){ // En el primer nivel la cantidad de asteriscos es mínimo empezando desde 1 .
+            ref+='*';
+        }
+        ref+='\n';
+    }
+    for(i=0;i<n2;i++){// Segundo nivel del árbol.
+        for(b=((n2+3)-i);b>0;b--){ // Diez espacios en blanco, disminuyo en 3 a compración del primer nivel.
+            ref+=' ';
+        }
+        for(a=0;a<((2*i)+7);a++){ // En este nivel la cantidad de asteriscos es más, empieza desde 7.
+            ref+='*';
+        }
+        ref+='\n';        
+    }
+    for(i=0;i<n3;i++){ // Tercer nivel del árbol.
+        for(b=(n3-i);b>0;b--){ // Siete espacios en blanco, disminuyo en 3 a comparacion del segundo nivel n2.
+            ref+=' ';
+        }
+        for(a=0;a<((2*i)+13);a++){ // Aumenta en 12 asteriscos, la cantidad de asteriscos empieza desde 13.
+            ref+='*';
+        }
+        ref+='\n';        
+    }
+    for (var t = 0; t < 5; t++) { //la longitud del tallo es la cantidad de iteraciones, en este caso 5.
+        for (var b = 0; b < 11; b++) { //once uniddades de espacio para dibujar el tallo.
+          ref += ' ';
+        }
+        ref += '***** \n'; //La longitud del tallo, un total de 5 asteriscos.
+      }
+    return ref;
+}
+
+//Imprimiendo resultados por consola
+var an= ArbolNavidad();
+console.log(an);
