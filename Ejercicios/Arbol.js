0df30c508bec0784652a233d5b8749cdc74c83eb
ridixcr <pabch.04.06.89@gmail.com>
2018-01-16 21:36:15 -0500

    se

--- Ejercicios/Arbol.js
+++ Ejercicios/Arbol.js
@@ -1,27 +1,27 @@
 function Arbol(n) {
-  var ref = '';
-  for (var i = 0; i < n; i++) {
-    for (var c = (n - i); c > 0; c--) {
-      ref += ' ';
+    var ref = '';
+    for (var i = 0; i < n; i++) {
+        for (var c = (n - i); c > 0; c--) {
+            ref += '-';
+        }
+        for (var j = 1; j <= ((2 * i) + 1); j++) {
+            ref += '*';
+        }
+        ref += '\n';
     }
-    for (var j = 1; j <= ((2 * i) + 1); j++) {
-      ref += '*';
+    for (var t = 0; t < (n - 1); t++) {
+        for (var r = 0; r < n; r++) {
+            ref += ' ';
+        }
+        ref += '* \n';
     }
-    ref += '\n';
-  }
-  for (var t = 0; t < (n - 1); t++) {
-    for (var r = 0; r < n; r++) {
-      ref += ' ';
+    for (var ra = 0; ra < 5; ra++) {
+        ref += '-';
     }
-    ref += '* \n';
-  }
-  for(var ra=0;ra<5;ra++){
-    ref += '-';
-  }  
-  for(var ra=1;ra<9;ra++){
-    ref += '*';
-  }
-  return ref;
+    for (var ra = 1; ra < 9; ra++) {
+        ref += '*';
+    }
+    return ref;
 }
 var a = Arbol(10);
 console.log(a);
