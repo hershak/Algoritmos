a9ca2328a7f338496ad7645f5707216b9b4ded8c
ridixcr <pabch.04.06.89@gmail.com>
2018-01-17 22:12:05 -0500

    pascal

--- Ejercicios/Pascal.js
+++ Ejercicios/Pascal.js
@@ -1,21 +1,21 @@
-function Pascal() {
-var fila = 10;
-  var lista = [1];
-  var ref='';
-  for (i = 0; i <= fila; i++) {
-    var memoria = [i];
-    for (j = 0; j < i; j++) {
-      if (j == 0 || j == (i - 1)) {
-        memoria[j] = 1;
-      } else {
-        memoria[j] = lista[j - 1] + lista[j];
-      }
+function Pascal(fila) {
+    var lista = [1];
+    var ref = '';
+    for (i = 1; i <= fila; i++) {
+        var memoria = new Array(i);
+        for (j = 0; j < i; j++) {
+            if (j == 0 || j == (i - 1)) {
+                memoria[j] = 1;
+            } else {
+                memoria[j] = lista[j - 1] + lista[j];
+            }
+            ref += memoria[j] + ' ';
+        }
+        lista = memoria;
+        ref += '\n';
     }
-    lista = memoria;
-    ref += '\n';
-  }
-  return ref;
+    return ref;
 }
 
-var L = Pascal();
+var L = Pascal(20);
 console.log(L);
