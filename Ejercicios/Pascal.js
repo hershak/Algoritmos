f1c13a2bb82f9222689649c2759cfb375725f4f4
hershak <gael.houl@gmail.com>
2018-01-17 20:46:40 -0500

    Pascal

--- Ejercicios/Pascal.js
+++ Ejercicios/Pascal.js
@@ -1,21 +1,21 @@
-function Pascal(lista,memoria) {
-  var lista = [];
-  var memoria = [];
-  for (i = 0; i < 5; i++) {
-    console.log('paso for');
-    if (i < 2) {
-      lista[i] = 1;
-    } else {
-      for (j = 0; j <= i; j++) {
-        if (j == 0 || j == i) {
-          memoria[j] = 1;
-        } else {
-          memoria[j] = lista[j - 1] + lista[j];
-        }
+function Pascal() {
+var fila = 10;
+  var lista = [1];
+  var ref='';
+  for (i = 0; i <= fila; i++) {
+    var memoria = [i];
+    for (j = 0; j < i; j++) {
+      if (j == 0 || j == (i - 1)) {
+        memoria[j] = 1;
+      } else {
+        memoria[j] = lista[j - 1] + lista[j];
       }
-      lista = memoria;
     }
+    lista = memoria;
+    ref += '\n';
   }
-  return lista;
+  return ref;
 }
 
+var L = Pascal();
+console.log(L);
